ee0afd387982773efc9ca9fc3235753431859e28
zeus <dhiogo.correa@itera.com.br>
2020-11-14 02:59:32 -0300

    fix2

--- parfait-api/src/main/java/com/megahack/parfait_api/controller/ProductController.java
+++ parfait-api/src/main/java/com/megahack/parfait_api/controller/ProductController.java
@@ -25,7 +25,7 @@ public class ProductController {
 
 	@RequestMapping(value = "/product/sample", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public List<Product> getSample(@RequestParam(required = false, value = "50") int size) {
+	public List<Product> getSample(@RequestParam int size) {
 		return productsService.getSample(size);
 	}
 
