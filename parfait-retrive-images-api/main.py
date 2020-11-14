d636056c6378813c1d1bb9533b71a006a700f6d2
Gustavo <gmpinatti@gmail.com>
2020-11-14 14:05:20 -0300

    retrive images api

---
+++ parfait-retrive-images-api/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI
+from keras.models import load_model
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import os
+import pandas as pd
+
+app = FastAPI()
+
+cos_similarities_df = pd.read_excel("df_recommendations.xlsx")
+
+@app.get("/")
+def retrieve_most_similar_products(img_id, nb_closest_images):
+
+    img_path = img_id + ".jpg"
+
+    closest_imgs = cos_similarities_df[img_path].sort_values(ascending=False)[1:nb_closest_images+1].index
+    
+    path_imgs_array = []
+
+    for img_index in closest_imgs:
+        path_imgs_array.append(cos_similarities_df[img_index].replace(".jpg", ""))
+    
+    return path_imgs_array
