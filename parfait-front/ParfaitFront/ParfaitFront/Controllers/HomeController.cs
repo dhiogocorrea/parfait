a020e73dbfa3d1c0f1c502f1f601e961990039f6
Vinicius <vinicius.ferreira@itera.com.br>
2020-11-15 00:09:49 -0300

    Login template

--- parfait-front/ParfaitFront/ParfaitFront/Controllers/HomeController.cs
+++ parfait-front/ParfaitFront/ParfaitFront/Controllers/HomeController.cs
@@ -24,6 +24,10 @@ namespace ParfaitFront.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
