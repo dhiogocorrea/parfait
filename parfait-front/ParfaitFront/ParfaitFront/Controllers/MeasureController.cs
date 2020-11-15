2fb79a68eea2e54ae290d72a8201c8a2c37fb789
Vinicius <vinicius.ferreira@itera.com.br>
2020-11-15 19:01:37 -0300

    measure controller

---
+++ parfait-front/ParfaitFront/ParfaitFront/Controllers/MeasureController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParfaitFront.Models;
+
+namespace ParfaitFront.Controllers
+{
+
+    public class MeasureController : Controller
+    {
+        private string tokenUser;
+        private readonly ILogger<HomeController> _logger;
+
+        public MeasureController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string stringSearch, string token)
+        {
+            return View();
+
+        }
+    }
+}
